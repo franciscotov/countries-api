5a4c549b825241c9820600802cc34317e38a9964
franciscotov <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2025-05-12 01:02:30 -0300

    fixinf bugs

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const { Country, Activity } = require('../db.js');
 
 router.get('/', async function(req, res, next){
-    res.send('hola')
+    res.send('alive.........')
 });
 router.post('/', async function(req, res, next){
     
